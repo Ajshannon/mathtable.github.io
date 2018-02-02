8e304c4d127c8180dcc48dfefb1f2b4642187775
AJ Shannon <anjoshannon@gmail.com>
2018-02-02 15:01:37 -0500

    I finally #$%@@#$ did it

---
+++ cart_logic.js
@@ -0,0 +1,61 @@
+// var availableBananas = 20;
+// var availableApples = 20;
+// var availableOranges = 3;
+
+// var selectedBananas = 1;
+// var selectedApples = 1;
+// var selectedOranges = 1;
+
+// var subtotalBananas = calculateProdTotal(BANANA_PRICE, selectedBananas);
+// var subtotalApples = calculateProdTotal(APPLE_PRICE, selectedApples);
+// var subtotalOranges = calculateProdTotal(ORANGE_PRICE, selectedOranges);
+
+// var subtotal = 0;
+// var shipping = 0;
+// var taxes = 0;
+// var total = 0;
+
+/* 1. Complete the function below to calculate product line 
+      Input: price of the product (double)
+      Input: quantity selected (integer)
+      Output: product line totals (double)
+*/
+
+function calculateProdTotal (price, quantity) {
+    return price * quantity;
+    
+}
+
+function calculateSubtotal (prodTotals) {
+    
+    return prodTotals[0] + prodTotals[1] + prodTotals[2];   // replace this!
+}
+
+function calculateTaxes(grossTotal, taxRate) {
+    taxes = subtotal * TAX_RATE;
+    return(taxes) ;   // replace this!
+}
+
+function calculateTotal (grossTotal, addedShipping, addedTax) {
+    total = subtotal + taxes + addedShipping;
+    return (total);   // replace this!
+}
+
+function updateProdAvailability(product, selected, availability) {
+
+let productAvailability = ""
+    if (selected > availability) {
+        Label = product + "OUT OF STOCK";
+        productAvailability = "OUT_OF_STOCK";
+    }   
+        else if (selected == availability) {
+        Label = product + "LIMITED STOCK";
+        productAvailability = "LIMITED_SUPPLY";
+    }
+        else if (selected < availability) {
+        Label = product + "IN STOCK";
+        productAvailability = "AVAILABLE";
+    }
+    updateProdAvailabilityLabel (product, Label, productAvailability)
+    return; 
+}
